f18cd1e94eefd085c43bde67d8f2c9a52e01e8c0
David Yan <idontwanttotalk@gmail.com>
2017-07-06 17:26:44 -0400

    general addition

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
 
+using namespace std;
+
+int main(){
+
+	return 0;
+}
 
--- room.hpp
+++ room.hpp
@@ -1,10 +1,27 @@
+#pragma once
+#include "Feature.hpp"
+#include "Object.hpp"
 #include <iostream>
-#include <cstdlib>
 #include <string>
+#include <vector>
+#include <fstream>
 
-using namespace std;
 
-int main(){
+class Room
+{
+public:
+	int visited;
+	std::string roomName;
+	std::string roomDesc = "AllRooms.txt";
+	std::vector<Room> roomList;
+	std::vector<Feature> featureList;
+	std::vector<Object> objectList;
+	Room(std::string rName);
+	Room();
+	~Room();
+	void printRooms();
+	void printFeatures();
+	void printObjects();
+	void readDesc(int i);
+};
 
-	return 0;
-}
