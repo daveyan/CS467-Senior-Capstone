11da6066354137e8c59e203390092abb951a0105
David Yan <idontwanttotalk@gmail.com>
2017-07-07 17:00:56 -0400

    testing new functionalities

--- main.cpp
+++ main.cpp
@@ -14,6 +14,10 @@ int main(){
 	r.readDesc(r.visited,"look");
 
 	//cout << r.roomDesc << endl;
+
+	Room r2("Main Room");
+	r2.readDesc(r2.visited,"look");
+	//cout << r2.roomDesc << endl;
 	
 
 	return 0;
