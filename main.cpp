8c4889fbcf364d51a639fb72252104d5e376a502
David Yan <idontwanttotalk@gmail.com>
2017-07-07 01:09:11 -0400

    running read desc twice

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 #include <cstdlib>
 #include <string>
 
+#include "room.hpp"
+
 using namespace std;
 
 int main(){
+
+	Room r("Room1");
+
+	r.readDesc(r.visited,"look");
+	r.readDesc(r.visited,"look");
+
+	//cout << r.roomDesc << endl;
+	
 
 	return 0;
 }
