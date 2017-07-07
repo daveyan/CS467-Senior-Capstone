1dba8311a6394b3c9e5e080af50a5e89cb8ecc44
David Yan <idontwanttotalk@gmail.com>
2017-07-07 01:09:00 -0400

    minor changes

--- feature.cpp
+++ feature.cpp
@@ -1,88 +1,14 @@
-#include "Room.hpp"
-#include <iostream>
+#include "feature.hpp"
 
 
-Room::Room() 
+
+Feature::Feature(std::string fName)
 {
-}
-
-Room::Room(std::string rName)
-{
-	roomName = rName;
-	visited = 0;
-}
-
-void Room::printRooms()
-{
-	for (int i = 0; i < static_cast<signed int>(roomList.size()); i++){
-		std::cout << roomList[i].roomName << std::endl;
-	}
-}
-
-void Room::printFeatures()
-{
-	for (int i = 0; i < static_cast<signed int>(featureList.size()); i++) {
-		std::cout << featureList[i].featureName << std::endl;
-	}
-}
-
-void Room::printObjects()
-{
-	for (int i = 0; i < static_cast<signed int>(objectList.size()); i++) {
-		std::cout << objectList[i].objectName << std::endl;
-	}
-}
-
-void Room::readDesc(int i)
-{
-	std::string readString;
-	std::ifstream roomFile;
-	roomFile.open(roomDesc);
-
-	if (roomFile.is_open()) {
-		while (!roomFile.eof()) {
-			std::getline(roomFile, readString);
-			if (i == 0) {
-				if (readString == "[start" + roomName + "long]") {
-					while (readString != "[end" + roomName + "long]") {
-						std::getline(roomFile, readString);
-						if (readString == "[end" + roomName + "long]") {
-							break;
-						}
-						else {
-							std::cout << readString << std::endl;
-						}
-					}
-
-				}
-				visited = 1;
-			}
-			else {
-				if (readString == "[start" + roomName + "short]") {
-					while (readString != "[end" + roomName + "short]") {
-						std::getline(roomFile, readString);
-						if (readString == "[end" + roomName + "short]") {
-							break;
-						}
-						else {
-							std::cout << readString << std::endl;
-						}
-					}
-
-				}
-			}
-
-
-		}
-		roomFile.close();
-	}
-	else {
-		std::cout << "Unable to open file\n";
-	}
+	featureName = fName;
 }
 
 
-
-Room::~Room()
+Feature::~Feature()
 {
 }
+
--- feature.hpp
+++ feature.hpp
@@ -1,27 +1,12 @@
 #pragma once
-#include "Feature.hpp"
-#include "Object.hpp"
-#include <iostream>
 #include <string>
-#include <vector>
-#include <fstream>
 
-
-class Room
+class Feature
 {
 public:
-	int visited;
-	std::string roomName;
-	std::string roomDesc = "AllRooms.txt";
-	std::vector<Room> roomList;
-	std::vector<Feature> featureList;
-	std::vector<Object> objectList;
-	Room(std::string rName);
-	Room();
-	~Room();
-	void printRooms();
-	void printFeatures();
-	void printObjects();
-	void readDesc(int i);
+	std::string featureName;
+	Feature(std::string fName);
+	~Feature();
+	
 };
 
