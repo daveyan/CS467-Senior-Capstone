4b919628e19c35aa0d3ee1df2d6166768a41709f
David Yan <idontwanttotalk@gmail.com>
2017-07-21 18:12:36 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/play.cpp.o"
+  "CMakeFiles/project.dir/Room.cpp.o"
+  "CMakeFiles/project.dir/Item.cpp.o"
+  "CMakeFiles/project.dir/Game.cpp.o"
+  "project.pdb"
+  "project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
