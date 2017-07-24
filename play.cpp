3925e26f400055cf1bf375aceabce004155bde10
David Yan <idontwanttotalk@gmail.com>
2017-07-24 01:17:15 -0400

    reader added to item

--- play.cpp
+++ play.cpp
@@ -6,8 +6,10 @@ int main() {
 	Game newGame;
 
 	Room r(1,"Room1");		//test room
+	Item i(1,"crowbar");
 
 	newGame.addRoom(r);		//adding test room
+	newGame.addItem(i);
 
     newGame.play();
     return 0;
